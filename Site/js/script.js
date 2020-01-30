7dc95faa90d68eb4c3224ee1eab81728bc066859
willemjanpattyn <willemjan.pattyn@gmail.com>
2020-01-30 15:21:18 +0100

    v3

--- Site/js/script.js
+++ Site/js/script.js
@@ -2,44 +2,10 @@ $(document).ready(function () {
   //TODO: remove init code
   var apiKey = 'de94f81ce9ab4cb4ba49ea8a87181ff9';
 
-  // var json = {
-  //   "imageUrl": "http://dtsl.ehb.be/~willem-jan.pattyn/Innovation%20week/upload/image-5e3190aee21b7.jpg"
-  // }
-
-  
-
-  // var url = "https://api.spoonacular.com/recipes/findByIngredients";
-  // $.ajax({
-  //   url: url + "?apiKey=" + apiKey + "&ingredients=tomato,onion,chicken",
-  //   contentType: 'application/json',
-  //   dataType: 'json',
-  //   context: document.body
-  // }).done(function (output) {
-  //   console.log(output);
-
-  //   output.forEach(r => {
-  //     elements = `<div class="card" style="width: 18rem; margin: 10px;">
-  //      <img class="card-img-top" src="${r.image}" alt="${r.title}" style="height: 200px;>
-  //      <div class="card-body">
-  //        <p class="card-text">${r.title}</p>
-  //        <p class="card-missing">Missing:</p>
-  //        <p class="card-missingIng"> 
-  //        `;
-  //     r.missedIngredients.forEach(i => {
-
-  //       elements += i.name + ", ";
-  //     });
-  //     elements += `</p>
-  //     <a href="details.html?id=${r.id}" class="btn btn-outline-primary btn-lg btn-block">Get more information</a>
-  //    </div>
-  //   </div>`
-  //     $(".card-columns").append(elements);
-  //   });
-  // });
-
   //Image upload
   //https://makitweb.com/how-to-upload-image-file-using-ajax-and-jquery/
   $("#fileUpload").change(function () {
+    $(".card").remove();
     console.log("uploading...");
     var fd = new FormData();
     var files = $('#fileUpload')[0].files[0];
@@ -97,10 +63,19 @@ $(document).ready(function () {
       //TODO: receive input from API and show recipes
       console.log(response);
 
-      //PLACEHOLDER
+      var ingredientClasses = response.image.images[0].classifiers[0].classes;
+      var ingredients = "";
+      ingredientClasses.forEach(c =>{
+        if(c.score >= 0.7){
+          ingredients += c.xclass + ","
+        }
+      });
+
+      console.log(ingredients);
+
       var url = "https://api.spoonacular.com/recipes/findByIngredients";
       $.ajax({
-        url: url + "?apiKey=" + apiKey + "&ingredients=tomato,onion,chicken",
+        url: url + "?apiKey=" + apiKey + "&ingredients=" + ingredients,
         contentType: 'application/json',
         dataType: 'json',
         context: document.body
@@ -115,9 +90,19 @@ $(document).ready(function () {
              <p class="card-missing">Missing:</p>
              <p class="card-missingIng"> 
              `;
-          r.missedIngredients.forEach(i => {
+          // r.missedIngredients.forEach(i => {
     
-            elements += i.name + ", ";
+          //   elements += i.name + ", ";
+          // });
+          r.missedIngredients.forEach(i => {
+
+            let l = r.missedIngredients[r.missedIngredients.length-1];
+            if(i.name == l.name){
+              elements += i.name;
+            }
+            else {
+              elements += i.name+ ", ";
+            }
           });
           elements += `</p>
           <a href="details.html?id=${r.id}" class="btn btn-outline-primary btn-lg btn-block">Get more information</a>
@@ -126,7 +111,6 @@ $(document).ready(function () {
           $(".card-columns").append(elements);
         });
       });
-      //PLACEHOLDER
     }
   });
 });
